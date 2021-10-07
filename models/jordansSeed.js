829070f7b7ba05411ab516a347fa01d177c03af2
Mohammed Faisal <mfaisal718@gmail.com>
2021-10-07 15:15:32 -0400

    Buy button completed

--- models/jordansSeed.js
+++ models/jordansSeed.js
@@ -1,20 +1,20 @@
 module.exports = [{
-    name: 'Jordan 6s Carmines',
-    description: 'An exclusive pair of the classic 6s in Carmine colorway',
-    img: 'https://imgur.com/M29q2gU',
-    price: 600,
-    qty: 3
-  }, {
-    name: 'Jordans Off-White 4s',
-    description: 'Excludive collaboration between Off-White and Jordans',
-    img: 'https://imgur.com/43iFZgt',
-    price: 1000,
-    qty: 3
-  }, {
-    name: 'Jordan 1s Carolina Blues',
-    description: 'The OG 1s decked out in MJs college Tarheel Blues',
-    img: 'https://imgur.com/IO5aORV',
-    price: 550,
-    qty: 3
-  }
+  name: 'Jordan 6s Carmines',
+  description: 'An exclusive pair of the classic 6s in Carmine colorway',
+  img: 'https://imgur.com/M29q2gU',
+  price: 600,
+  qty: 3
+}, {
+  name: 'Jordans Off-White 4s',
+  description: 'Excludive collaboration between Off-White and Jordans',
+  img: 'https://imgur.com/43iFZgt',
+  price: 1000,
+  qty: 3
+}, {
+  name: 'Jordan 1s Carolina Blues',
+  description: 'The OG 1s decked out in MJs college Tarheel Blues',
+  img: 'https://imgur.com/IO5aORV',
+  price: 550,
+  qty: 3
+}
 ]
